7020997b78accf0ba9a00d9af5f44a59a1c0d4c6
valemun <valeria.munoz@hotmail.com>
2021-03-08 15:25:45 -0800

    inicializando servidor

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+//Configuaciones
+app.set("port", 3000);
+
+//Middlewares
+app.use(morgan("dev")); //mas detalle "combined"
+app.use(express.json());
+
+//Rutas
+app.use(require("./routes/rutas"));
+//app.use(require("./routes/rutasAdmin"));
+
+
+//Empezando el servidor
+app.listen(app.get("port"));
+console.log("mi primer servidor");
