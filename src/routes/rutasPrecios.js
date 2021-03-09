f774dbe3edf35e89119d772d1d63eccc4432a572
valemun <valeria.munoz@hotmail.com>
2021-03-08 16:36:57 -0800

    puro acomodo

--- src/routes/rutasPrecios.js
+++ src/routes/rutasPrecios.js
@@ -1,22 +1,11 @@
 const {Router} = require("express");
 const router = Router();
-//const express = require("express");
-//const router = express.Router();
+
 
 router.get("/", (req, res) =>{
     res.send("si funciona!"); //.send es para mandar string
 });
 
-router.get("/home", (req, res) =>{
-    res.json({"name": "John Doe"}); //.json es para json
-});
-
-router.get("/json", (req, res) =>{
-    const data = {"name": "John Doe",
-    "id": "1234"
-    }
-    res.json(data); //.json es para json
-});
 
 module.exports = router;
 
--- src/routes/rutasUsuarios.js
+++ src/routes/rutasUsuarios.js
@@ -1,14 +1,8 @@
 const {Router} = require("express");
 const router = Router();
-//const express = require("express");
-//const router = express.Router();
 
 router.get("/", (req, res) =>{
     res.send("si funciona!"); //.send es para mandar string
-});
-
-router.get("/home", (req, res) =>{
-    res.json({"name": "John Doe"}); //.json es para json
 });
 
 module.exports = router;
