4c2ccbf3d25bf3d3c2e369deccd1798a7b1f9bd4
valemun <valeria.munoz@hotmail.com>
2021-03-10 10:39:41 -0800

    rutas de contenido con gets y parametros

--- src/routes/rutasContenidos.js
+++ src/routes/rutasContenidos.js
@@ -4,13 +4,17 @@ const router = Router();
 //const router = express.Router();
 
 const contenido = require("./dataContenido.json");
+//console.log(contenido);
 
 router.get("/", (req, res) =>{
     res.json(contenido);
 });
 
-router.get("/{idContenido}", (req, res) =>{
-    res.json({"name": "John Doe"}); //.json es para json
+router.get("/:id", (req, res) =>{
+   const {id} = req.params;
+    
+
+   console.log(id);
 });
 
 module.exports = router;
