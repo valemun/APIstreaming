08c49b118018570403fb8490f6a3d5bd073c57c9
valemun <valeria.munoz@hotmail.com>
2021-03-21 17:14:00 -0700

    rutas contenido

--- src/routes/rutasContenidos.js
+++ src/routes/rutasContenidos.js
@@ -35,25 +35,81 @@ router.get("/genero/:genre", (req, res) =>{
 
  router.get("/seleccionarCatalogo/:edad", (req, res) =>{
     const {edad} = req.params;
-    
+    const peliculas = [];
+
     if(edad < 13){
        
-        contenido.forEach(contenido => {
-            if(contenido.rating == "g"){
-                res.json(contenido);
+
+        contenido.forEach(cont => {
+            if(cont.rating == "g" || cont.rating == "pg"){
+                peliculas.push(cont);
             }
         });
 
     }
-    if (edad > 13){
 
-        contenido.forEach(contenido => {
-            if(contenido.rating == "g" || contenido.rating == "pg-13"){
-                res.json(contenido);
+    if (edad >= 13 && edad < 18){
+
+        contenido.forEach(cont => {
+            if(cont.rating != "r" || cont.rating != "nc-17"){
+                peliculas.push(cont);
             }
         });
 
     }
+
+    if (edad >= 18){
+
+        contenido.forEach(cont => {
+            if(cont.genero != "infantil"){
+                peliculas.push(cont);
+            }
+        });
+
+    }
+
+    res.send(peliculas);
+    
+ });
+
+ router.get("/seleccionarContenido/:suscripcion", (req, res) =>{
+    const {suscripcion} = req.params;
+    const peliculas = [];
+
+    if(suscripcion == "kids"){
+       
+
+        contenido.forEach(cont => {
+            if(cont.rating == "g" || cont.rating == "pg" && cont.tipo == "suscripcion"){
+                peliculas.push(cont);
+            }
+        });
+
+    }
+
+    if(suscripcion == "familiar"){
+       
+
+        contenido.forEach(cont => {
+            if(cont.tipo == "suscripcion"){
+                peliculas.push(cont);
+            }
+        });
+
+    }
+
+    if(suscripcion == "premium"){
+       
+
+        contenido.forEach(cont => {
+            if(cont.tipo == "premium"){
+                peliculas.push(cont);
+            }
+        });
+
+    }
+
+    res.send(peliculas);
     
  });
 
