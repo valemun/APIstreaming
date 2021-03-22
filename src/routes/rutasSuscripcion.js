69c6769e019c5d2c9ff2ef82374291f8528d65d8
maria.montex <maria.montes@cetys.edu.mx>
2021-03-21 17:08:46 -0700

    rutas suscripciones

--- src/routes/rutasSuscripcion.js
+++ src/routes/rutasSuscripcion.js
@@ -1,30 +1,46 @@
 const {Router} = require("express");
 const router = Router();
-
-const contenido = require("./dataSuscripcion.json");
-//console.log(suscripcion);
+const suscripcion = require("./dataSuscripcion.json");
+//console.log(suscripcion); 
 
 router.get("/", (req, res) =>{
-    res.send("si funciona!"); //.send es para mandar string
+    res.send(suscripcion); //.send es para mandar string
 });
 
 router.get("/:id", (req, res) => {
     const { id } = req.params;
-    contenido.forEach(cont => {
+
+    suscripcion.forEach(cont => {
         if (cont.id == id) {
             res.json(cont);
         }
     })
 });
 
-router.get("/seleccionarTipo/:tipo", (req, res) => {
+router.get("/Tipo/:tipo", (req, res) => {
     const {tipo} = req.params;
 
+    suscripcion.forEach (suscripcion =>{
+        if (suscripcion.tipo == tipo) {
+                res.json (suscripcion);
+                console.log (suscripcion.tipo, suscripcion.precio);
+        }
+    });
 
 });
 
-router.get("/seleccionarPrecio")
+//esta mal, solo es guia qwq
+router.get("/numusuarios/:usuarios", (req, res) => {
+    const {usuarios} = req.params;
 
+    suscripcion.forEach (suscripcion =>{
+        if (suscripcion.usuarios == usuarios) {
+                res.json (suscripcion);
+                console.log (suscripcion.tipo, suscripcion.usuarios);
+        }
+    });
+
+});
 
 module.exports = router;
 
